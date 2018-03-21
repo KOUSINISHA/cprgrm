ec4a7948a4f624ba1dcdb8fb4e32c760913f9e40
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-21 16:27:14 +0530

    sorted.c

---
+++ sorted.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+printf("Enter the number of array");
+scanf("%d,&s");
+printf("Enter the number of integer");
+scanf("%d,&d");
+{
+printf("print the sorted array");
+}
+getch();
+}
