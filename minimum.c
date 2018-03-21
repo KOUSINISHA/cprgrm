25e38d8b269187f556f413b7346f39b13c12fa37
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-21 16:19:29 +0530

    minimum.c

---
+++ minimum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int=i,j;
+printf("Enter the number of element");
+scanf("%d,&s");
+printf("print the minimum element");
+scanf("%d,&s");
+{
+for(i=0;1<100;i++);
+printf("print the number of minimum element");
+}
+getch();
+}
