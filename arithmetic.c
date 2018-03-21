89ccf28de8a13fabb6107b954dcc84b465d9a191
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-21 16:01:22 +0530

    arithmatic.c

---
+++ arithmetic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+printf("Enter the sum of number");
+scanf("%d,&N");
+printf("Enter the sum of arithmatic");
+scanf("%d,&A");
+printf("Enter the of data");
+scanf("%d,&D");
+{
+getch()
+}
+}
