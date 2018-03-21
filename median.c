f201bd3ee590fc81ac4929d2d0d9053cc2cebc47
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-21 16:39:20 +0530

    median.c

---
+++ median.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include<conio.h>
+{
+(int a[], int n)
+int sum = 0;
+for (int i = 0; i < n; i++) 
+sum += a[i];
+}
+(int a[], int n)
+{
+sort(a, a+n);
+if (n % 2 != 0)
+}
+int main()
+{
+    int a[] = { 1, 3, 4, 2, 7, 5, 8, 6 };
+    printf("Enter the element");
+    scanf("%d,&s");
+    printf("print the median element");
+    scanf("%d,&s");
+    return 0;
+}
