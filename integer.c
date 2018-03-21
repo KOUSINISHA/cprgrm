7aae63462cbb8282fd0dcab965adc01e0059cd9e
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-21 16:12:41 +0530

    integer.c

---
+++ integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int=i,j;
+printf("Enter the number of element");
+scanf("%d,&s");
+printf("print the maximum element");
+scanf("%d,&s");
+{
+for(i=0;1<100;i++);
+printf("print the number of maximum element");
+}
+getch();
+}
